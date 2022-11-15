38ce9b3dc04b0c65f7c786708e3a1288fe266ec4
Rushabh Agarwal <rushabh.agarwal9@gmail.com>
2022-11-15 20:41:39 +0530

    Writing output to the file for simulator

--- simulator/sample.py
+++ simulator/sample.py
@@ -1,26 +1,35 @@
-import time
-from datetime import datetime
+from csv import writer
 import pandas as pd
 
+file_name = input("Input File Name")
+
+with open(f'simulator/output/{file_name}_day_wise.csv', 'w') as file_object:
+    writer_object = writer(file_object)
+    writer_object.writerow(['Date', 'Cash Available', 'Cash Invested', 'Open', 'High', 'Low', 'Close'])
+
+with open(f'simulator/output/{file_name}_order_wise.csv', 'w') as file_object:
+    writer_object = writer(file_object)
+    writer_object.writerow(['OrderID', 'Buy Date', 'Buy Price', 'Sell Date', 'Sell Price'])
+
 # CSV to DataFrame
-df = pd.read_csv('simulator/NIFTYBEES2.csv', header=None)
+df = pd.read_csv(f'simulator/Input/{file_name}.csv', header=None)
 date_price_list: list = df.values.tolist()
 
 # date_price_list.reverse()
 
 BUYING_MARGIN = 0.99
 SELLING_MARGIN = 1.01
-UNITS = 150
-equity_margin = 10000
-NIFTY_BEES_LTP = 30
+equity_margin = 100000
+NIFTY_BEES_LTP = date_price_list[0][1]
+NIFTY_BEES_CTP = date_price_list[0][1]
+UNITS = round(equity_margin/(NIFTY_BEES_CTP * 2))
 
 orders = {}
 order_id = 0
 
 for date_prices in date_price_list:
     date = date_prices[0]
-
-    prices = [date_prices[1], date_prices[4]]
+    prices = date_prices[1:5]
     for price in prices:
         NIFTY_BEES_CTP = price
         if NIFTY_BEES_CTP <= (NIFTY_BEES_LTP * BUYING_MARGIN):
@@ -34,29 +43,43 @@ for date_prices in date_price_list:
                     "sell_price": NIFTY_BEES_CTP * SELLING_MARGIN,
                     "sold": False
                 }
+                # Update LTP on Buy
+                NIFTY_BEES_LTP = NIFTY_BEES_CTP
 
-        for id, order in orders.items():
+        for o_id, order in orders.items():
             if order["sold"] is False and NIFTY_BEES_CTP >= order["sell_price"]:
-                equity_margin = equity_margin + (NIFTY_BEES_CTP * UNITS)
+                equity_margin = equity_margin + (order["sell_price"] * UNITS)
 
                 order["sold"] = True
                 order["sell_date"] = date
-                orders[id] = order
+                orders[o_id] = order
 
-        NIFTY_BEES_LTP = NIFTY_BEES_CTP
+        # Update LTP on Open or Close.
+        if price == date_prices[1] or price == date_prices[4]:
+            NIFTY_BEES_LTP = price
 
     invested = 0
     for oid, order in orders.items():
         if order["sold"] is False:
             invested = invested + NIFTY_BEES_CTP * UNITS
-        if order['buy_date'] == date:
-            print(f"OrderId: {oid} Order Details: {order}")
-        if 'sell_date' in order and order['sell_date'] == date and order['sell_date'] != order['buy_date']:
-            print(f"OrderId: {oid} Order Details: {order}")
-    print(f"date: {date}, equity_margin: {equity_margin}, invested: {invested}, ctp: {NIFTY_BEES_CTP}")
+        # if order['buy_date'] == date:
+        #     print(f"OrderId: {oid} Order Details: {order}")
+        # if 'sell_date' in order and order['sell_date'] == date and order['sell_date'] != order['buy_date']:
+        #     print(f"OrderId: {oid} Order Details: {order}")
+
+    with open(f'simulator/output/{file_name}_day_wise.csv', 'a') as file_object:
+        writer_object = writer(file_object)
+        writer_object.writerow([date, equity_margin, invested, *date_prices[1:5]])
 
 
-print(f"Equity margin: {equity_margin}")
-print(f"Amount invested: {invested}")
+for oid, order in orders.items():
+    with open(f'simulator/output/{file_name}_order_wise.csv', 'a') as file_object:
+        writer_object = writer(file_object)
+        if order['sold']:
+            writer_object.writerow(
+                [oid, order['buy_date'], order['buy_price'], order['sell_date'], order['sell_price']])
+        else:
+            writer_object.writerow(
+                [oid, order['buy_date'], order['buy_price'], "na", order['sell_price']])
 
 
